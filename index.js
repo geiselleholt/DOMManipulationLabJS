cab7e3208947defb35d57354e5521128dc64a699
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-25 14:10:34 -0600

    changed boiler plate code from codesandbox-ese to vscode-ish, copied code over

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-import "./styles.css";
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+let mainEL = document.querySelector("main");
+mainEL.style.backgroundColor = "var(--main-bg)";
+mainEL.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEL.classList.add("flex-ctr");
+
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+menuLinks.forEach((link) => {
+  let linkEl = document.createElement("a");
+  linkEl.setAttribute("href", link.href);
+  linkEl.textContent = link.text;
+  topMenuEl.appendChild(linkEl);
+});
