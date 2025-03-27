570d4348ff1465a9436e224b3980125905df9d62
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-27 15:22:03 -0600

    Part 3 done

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 // Menu data structure
 var menuLinks = [
-  { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  { text: `about`, href: `/about` },
+  { text: `catalog`, href: `/catalog` },
+  { text: `orders`, href: `/orders` },
+  { text: `account`, href: `/account` },
 ];
 
-let mainEL = document.querySelector("main");
-mainEL.style.backgroundColor = "var(--main-bg)";
-mainEL.innerHTML = "<h1>DOM Manipulation</h1>";
-mainEL.classList.add("flex-ctr");
+let mainEL = document.querySelector(`main`);
+mainEL.style.backgroundColor = `var(--main-bg)`;
+mainEL.innerHTML = `<h1>DOM Manipulation</h1>`;
+mainEL.classList.add(`flex-ctr`);
 
-let topMenuEl = document.getElementById("top-menu");
-topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-topMenuEl.classList.add("flex-around");
+let topMenuEl = document.getElementById(`top-menu`);
+topMenuEl.style.height = `100%`;
+topMenuEl.style.backgroundColor = `var(--top-menu-bg)`;
+topMenuEl.classList.add(`flex-around`);
 
 menuLinks.forEach((menuLink) => {
-  let linkEl = document.createElement("a");
-  linkEl.setAttribute("href", menuLink.href);
+  let linkEl = document.createElement(`a`);
+  linkEl.setAttribute(`href`, menuLink.href);
   linkEl.textContent = menuLink.text;
   topMenuEl.appendChild(linkEl);
 });
+
+let subMenuEl = document.getElementById(`sub-menu`);
+subMenuEl.style.height = `100%`;
+subMenuEl.style.backgroundColor = `var(--sub-menu-bg)`;
+subMenuEl.classList.add(`flex-around`);
+subMenuEl.style.position = `absolute`;
+subMenuEl.style.top = '0';
+
