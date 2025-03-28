5cf3b3ff4fff126f70f015bcd0a157788b36ebdb
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-27 20:32:44 -0600

    Part 4 done, except the toggle doesn't work after adding the remove active code

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-// Menu data structure
 var menuLinks = [
-  { text: `about`, href: `/about` },
-  { text: `catalog`, href: `/catalog` },
-  { text: `orders`, href: `/orders` },
-  { text: `account`, href: `/account` },
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
 ];
 
 let mainEL = document.querySelector(`main`);
@@ -28,5 +50,23 @@ subMenuEl.style.height = `100%`;
 subMenuEl.style.backgroundColor = `var(--sub-menu-bg)`;
 subMenuEl.classList.add(`flex-around`);
 subMenuEl.style.position = `absolute`;
-subMenuEl.style.top = '0';
+subMenuEl.style.top = "0";
 
+let topMenuLinks = topMenuEl.querySelectorAll("a");
+topMenuEl.addEventListener("click", handleTopMenuEl);
+
+function handleTopMenuEl(e) {
+  e.preventDefault();
+
+  if (e.target.tagName !== "A") {
+    return;
+  } else {
+    console.log(`You clicked ${e.target.textContent}`);
+  }
+
+  topMenuLinks.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  e.target.classList.toggle("active");
+}
