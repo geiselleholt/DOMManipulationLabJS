457a3be280f932c35e599825c8da157ec4210a42
Geiselle Holt <geiselleholt@hotmail.com>
2025-03-27 23:54:19 -0600

    All of Part 5 done except toggle

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 var menuLinks = [
-  { text: "about", href: "/about" },
+  { text: `about`, href: `/about` },
   {
-    text: "catalog",
-    href: "#",
+    text: `catalog`,
+    href: `#`,
     subLinks: [
-      { text: "all", href: "/catalog/all" },
-      { text: "top selling", href: "/catalog/top" },
-      { text: "search", href: "/catalog/search" },
+      { text: `all`, href: `/catalog/all` },
+      { text: `top selling`, href: `/catalog/top` },
+      { text: `search`, href: `/catalog/search` },
     ],
   },
   {
-    text: "orders",
-    href: "#",
+    text: `orders`,
+    href: `#`,
     subLinks: [
-      { text: "new", href: "/orders/new" },
-      { text: "pending", href: "/orders/pending" },
-      { text: "history", href: "/orders/history" },
+      { text: `new`, href: `/orders/new` },
+      { text: `pending`, href: `/orders/pending` },
+      { text: `history`, href: `/orders/history` },
     ],
   },
   {
-    text: "account",
-    href: "#",
+    text: `account`,
+    href: `#`,
     subLinks: [
-      { text: "profile", href: "/account/profile" },
-      { text: "sign out", href: "/account/signout" },
+      { text: `profile`, href: `/account/profile` },
+      { text: `sign out`, href: `/account/signout` },
     ],
   },
 ];
@@ -50,44 +50,45 @@ subMenuEl.style.height = `100%`;
 subMenuEl.style.backgroundColor = `var(--sub-menu-bg)`;
 subMenuEl.classList.add(`flex-around`);
 subMenuEl.style.position = `absolute`;
-subMenuEl.style.top = "0";
+subMenuEl.style.top = `0`;
 
-let topMenuLinks = topMenuEl.querySelectorAll("a");
-topMenuEl.addEventListener("click", handleTopMenuEl);
+let topMenuLinks = topMenuEl.querySelectorAll(`a`);
+topMenuEl.addEventListener(`click`, handleTopMenuEl);
 
 function handleTopMenuEl(e) {
   e.preventDefault();
 
-  if (e.target.tagName !== "A") {
+  if (e.target.tagName !== `A`) {
     return;
   } else {
     console.log(`You clicked ${e.target.textContent}`);
   }
 
   topMenuLinks.forEach((link) => {
-    link.classList.remove("active");
+    link.classList.remove(`active`);
   });
 
-  if (e.target.classList.contains("active") == false) {
-    e.target.classList.add("active");
+  if (e.target.classList.contains(`active`) == false) {
+    e.target.classList.add(`active`);
     menuLinks.forEach((link) => {
       if (link.text == e.target.textContent) {
         let subLinks2 = link.subLinks;
         console.log(subLinks2);
         if (subLinks2) {
-          subMenuEl.style.top = "100%";
+          subMenuEl.style.top = `100%`;
+          buildSubmenu(subLinks2);
         } else {
-          subMenuEl.style.top = "0";
+          subMenuEl.style.top = `0`;
+          mainEL.innerHTML = `<h1>ABOUT</h1>`;
         }
-        buildSubmenu(subLinks2);
       }
     });
   } else {
-    e.target.classList.remove("active");
+    e.target.classList.remove(`active`);
   }
 
   function buildSubmenu(subLinks) {
-    subMenuEl.innerHTML = "";
+    subMenuEl.innerHTML = ``;
     subLinks.forEach((link) => {
       let subLinkEl = document.createElement(`a`);
       subLinkEl.setAttribute(`href`, link.href);
@@ -97,6 +98,27 @@ function handleTopMenuEl(e) {
     });
   }
 
-  console.log("Active class toggled:", e.target.classList.contains("active"));
-  console.log("Current active element:", e.target);
+  subMenuEl.addEventListener(`click`, handleSubMenuEl);
+
+  function handleSubMenuEl(e) {
+    e.preventDefault();
+    if (e.target.tagName !== `A`) {
+      return;
+    } else {
+      console.log(`You clicked ${e.target.textContent}`);
+    }
+
+    subMenuEl.style.top = `0`;
+
+    topMenuLinks.forEach((link) => {
+      link.classList.remove(`active`);
+    });
+
+    if (e.target.textContent !== `about`) {
+      mainEL.innerHTML = e.target.textContent.toUpperCase();
+    }
+  }
+
+  console.log(`Active class toggled:`, e.target.classList.contains(`active`));
+  console.log(`Current active element:`, e.target);
 }
